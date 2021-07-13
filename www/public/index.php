c3f6a14f083439bc4ddd25af37df913a30e1c27a
marckvaldo <marckvaldo@hotmail.com>
2021-07-13 09:55:31 -0300

    start charge

---
+++ www/public/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Docker com php</title>
+</head>
+<body>
+
+
+<div style="background-color: #f0f0f0; height:20vh; display: flex; flex-direction: row; justify-content: center;">
+    <img src="imagens/docker.png">
+    <img src="imagens/laravel.png">
+</div>
+<br/>
+<div>
+    <?php echo phpinfo();   ?>
+</div>
+
+</body>
+</html>
